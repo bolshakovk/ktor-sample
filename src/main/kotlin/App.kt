dda3dbe420dc06afe661dabe6402a326d126dab1
bolshakovk <bolshakovk1234@gmail.com>
2021-11-27 09:56:51 +0300

    it works

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,25 +1,21 @@
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
-import io.ktor.client.features.*
-import io.ktor.client.features.get
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.http.*
-import java.net.HttpURLConnection
+import org.json.JSONObject
 import java.net.URL
-
-
-
 
 suspend fun main() {
     val client = HttpClient(CIO)
-    val URL = URL("https://www.nytimes.com/")
-    val response: HttpResponse = client.request("https://www.nytimes.com/"){
-        method = HttpMethod.Get
-    }
-    val response: HttpResponse = client.get("https://www.nytimes.com/")
-    client.close()
+    val urlString = URL("https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=34a4801037bcd9496bca871a0688a1d6")
+    val response: HttpResponse = client.get(urlString)
+    println(response.readText())
 
+    val jsObject = JSONObject(response.readText())
+
+    println("Город: ${jsObject.getString("name")}, \nТемпература: ${jsObject.getJSONArray("weather")}")
+
+    client.close()
 }
 class App {
 }
